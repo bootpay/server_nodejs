733b6303faf0456635ade4a2b1e866cbd083ffb8
Gosomi <gosomi@forelf.com>
2018-05-29 11:22:58 +0900

    정기 결제까지 완료

---
+++ test/access_token.js
@@ -0,0 +1,12 @@
+var BootpayRest = require('../lib/bootpay');
+
+BootpayRest.setConfig(
+    '59bfc738e13f337dbd6ca48a',
+    'FQj3jOvQYp053nxzWxHSuw+cq3zUlSWZV2ec/8fkiyA=',
+    'development'
+);
+
+
+BootpayRest.getAccessToken(function (data) {
+    console.log(data);
+});
