2831a947169146bf611d98d547bb363fb8097eb1
Gosomi <gosomi@forelf.com>
2018-06-27 15:26:19 +0900

    정기 결제 예약 추가

---
+++ test/subscribe_billing_reserve.js
@@ -0,0 +1,22 @@
+var BootpayRest = require('../lib/bootpay');
+
+BootpayRest.setConfig(
+    '59bfc738e13f337dbd6ca48a',
+    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+    'development'
+);
+
+
+BootpayRest.getAccessToken()
+    .then(function (data) {
+        BootpayRest.subscribeBillingReserve(
+            '5b025b33e13f33310ce560fb',
+            '정기결제입니다.',
+            1000,
+            (new Date()).getTime(),
+            ((new Date()).getTime() + 10000) / 1000,
+            "https://dev-api.bootpay.co.kr/callback"
+        ).then(function (data) {
+            console.log(data);
+        });
+    });
