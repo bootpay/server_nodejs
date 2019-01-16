bc7dcea5a501773d7e457239a4541c2a84396999
Gosomi <gosomi@forelf.com>
2019-01-16 14:55:38 +0900

    정기결제 카드 요청 추가

---
+++ test/request_subscribe_rest.js
@@ -0,0 +1,23 @@
+var BootpayRest = require('../lib/bootpay');
+
+BootpayRest.setConfig(
+    '59bfc738e13f337dbd6ca48a',
+    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+    'development'
+);
+
+
+BootpayRest.getAccessToken().then(function (data) {
+    BootpayRest.getSubscribeBillingKey({
+        orderId: (new Date()).getTime(),
+        pg: 'nicepay',
+        name: '정기결제 30일권',
+        cardNo: '[ 카드 번호 ]',
+        cardPw: '[ 카드 비밀번호 앞 2자리 ]',
+        expireYear: '[ 카드 만료 연도 ]',
+        expireMonth: '[ 카드 만료 월 ]',
+        identifyNumber: '[ 카드 소유주 생년월일 혹은 법인 번호 ]'
+    }).then(function (data) {
+        console.log(data);
+    });
+});
