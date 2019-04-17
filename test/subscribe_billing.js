25c04f0574831f7d25b0856a0ed5a6f1024be73c
Gosomi <gosomi@forelf.com>
2019-04-17 16:48:15 +0900

    정기결제 사용자 정보 추가 입력 하는 field 추가

--- test/subscribe_billing.js
+++ test/subscribe_billing.js
@@ -8,9 +8,20 @@ BootpayRest.setConfig(
 
 
 BootpayRest.getAccessToken()
-.then(function(data) {
-    BootpayRest.subscribeBilling('5b025b33e13f33310ce560fb', '정기결제입니다.', 1000, (new Date()).getTime(), [])
-    .then(function(data) {
-        console.log(data);
-    });
-});
+    .then(
+        // Access Token을 가져왔을 때 처리
+        function (data) {
+            BootpayRest.subscribeBilling('5b025b33e13f33310ce560fb', '정기결제입니다.', 1000, (new Date()).getTime(), [], {
+                username: '홍길동',
+                phone: '010-0000-0000',
+                address: '서울특별시 구로구'
+            })
+                .then(function (data) {
+                    console.log(data);
+                });
+        },
+        // Access Token을 가져오는데 실패한 경우
+        function (data) {
+            console.log(data);
+        }
+    );
