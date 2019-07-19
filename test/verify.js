16fbb4539c12fe7ca1e2fedc89a3ca773f26dc2b
Gosomi <gosomi@forelf.com>
2019-07-19 13:48:23 +0900

    development 옵션 삭제

--- test/verify.js
+++ test/verify.js
@@ -2,8 +2,7 @@ var BootpayRest = require('../lib/bootpay');
 
 BootpayRest.setConfig(
     '59bfc738e13f337dbd6ca48a',
-    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
-    'development'
+    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0='
 );
 
 
