28a11eb54c7662f5e2f102d52337e870a0e4bc97
Gosomi <gosomi@forelf.com>
2020-03-30 17:54:10 +0900

    user token 가져오기 배포

---
+++ test/get_user_token.js
@@ -0,0 +1,23 @@
+var BootpayRest = require('../lib/bootpay');
+
+BootpayRest.setConfig(
+    '59bfc738e13f337dbd6ca48a',
+    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0='
+);
+
+BootpayRest.getAccessToken().then(function (tokenData) {
+    if (tokenData.status === 200) {
+        BootpayRest.getUserToken({
+            user_id: '[[ user_id ]] ( 필수 )',
+            email: 'bootpay@bootpay.co.kr', // 필수
+            name: '[[ 사용자 명 ]] ( 선택 )',
+            gender: '[[ 0 - 여자, 1 - 남자 ]] ( 선택 )',
+            birth: ' [[ 생년월일 ( 6자리) ]] ( 선택 )',
+            phone: ' [[ 전화번호 ]] ( 페이앱의 경우 필수 )'
+        }).then(function (data) {
+            console.log(data);
+        });
+    } else {
+        console.log('error!')
+    }
+});
